d417dfff8fbc5e988ffcd7f79a287f9e0d1ad3f8
newuser <popovskaarisha123@gmail.com>
2025-07-26 22:58:25 +0300

    Дз сделано

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,44 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.time.LocalDate;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        checkLeapYear(2023);
+        checkLeapYear(2024);
+        suggestAppVersion(0, 2014);
+        suggestAppVersion(1, 2020);
+        System.out.println("Потребуется дней: " + calculateDeliveryDays(95));
+        System.out.println("Потребуется дней: " + calculateDeliveryDays(10));
+        System.out.println("Потребуется дней: " + calculateDeliveryDays(50));
+        System.out.println("Потребуется дней: " + calculateDeliveryDays(150));
+    }
+ // задача 1
+    public static void checkLeapYear(int year) {
+        boolean isLeap = (year % 400 == 0) || (year % 100 != 0 && year % 4 == 0);
+        if (isLeap) {
+            System.out.println(year + " год — високосный год");
+        } else {
+            System.out.println(year + " год — невисокосный год");
+        }
+    }
+    // задача 2
+    public static void suggestAppVersion(int osType, int deviceYear) {
+        int currentYear = LocalDate.now().getYear();
+        String osName = osType == 0 ? "iOS" : "Android";
+        String version = deviceYear < currentYear ? "облегченную" : "обычную";
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("Установите " + version + " версию приложения для " + osName + " по ссылке");
+    }
+    // задача 3
+    public static int calculateDeliveryDays(int deliveryDistance) {
+        if (deliveryDistance >= 100) {
+            return -1;
+        } else if (deliveryDistance >= 60) {
+            return 3;
+        } else if (deliveryDistance >= 20) {
+            return 2;
+        } else {
+            return 1;
         }
     }
 }
